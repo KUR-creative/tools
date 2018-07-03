b069369be0087f17845c3bff33447420cddfe1af
kur <rhdnfka94@gmail.com>
2018-07-03 21:30:07 +0900

    Add look&decide

--- manual_classifier.py
+++ manual_classifier.py
@@ -1,11 +1,22 @@
 import cv2, h5py
+
+def look_and_decide(image,
+                    window_title='o: good crop / x: bad crop'):
+    while True:
+        cv2.imshow(window_title,image)
+        key = cv2.waitKey(1) & 0xFF
+        if (key == ord('o') or 
+            key == ord('x') or 
+            key == ord('q')):
+            return chr(key)
+
 def main():
     # load
     # look_and_decide
     with h5py.File('./mini_128x_1crop_32.h5','r') as f:
         images = f['images']
         for img in images:
-            cv2.imshow('img',img);cv2.waitKey(0)
+            print(look_and_decide(img))
 
 if __name__ == '__main__':
     main()
