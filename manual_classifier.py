27d0caebbab6dc0310bf310fb9ffb9d168b43567
kur <rhdnfka94@gmail.com>
2018-07-03 21:35:51 +0900

    Add commands keys

--- manual_classifier.py
+++ manual_classifier.py
@@ -1,12 +1,14 @@
 import cv2, h5py
 
 def look_and_decide(image,
-                    window_title='o: good crop / x: bad crop'):
+                    window_title='o x 4 6 q'):
     while True:
         cv2.imshow(window_title,image)
         key = cv2.waitKey(1) & 0xFF
-        if (key == ord('o') or 
-            key == ord('x') or 
+        if (key == ord('o') or # good crop
+            key == ord('x') or # bad crop
+            key == ord('6') or # next
+            key == ord('4') or # prev
             key == ord('q')):
             return chr(key)
 
