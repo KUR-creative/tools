8fef691c7317cbff4c132ba641e39f6a0c8bccff
kur <rhdnfka94@gmail.com>
2018-07-04 00:48:27 +0900

    Add o,x cmd functionality and print_state

--- manual_classifier.py
+++ manual_classifier.py
@@ -12,6 +12,9 @@ def look_and_decide(image,
             key == ord('q')):  # exit
             return chr(key)
 
+def print_state(ox_list, idx, num_checked, num_imgs):
+    print(ox_list[idx], ':', idx,'/',num_imgs, ' | ', 
+          num_checked,'/',num_imgs)
 def main():
     # load
     # look_and_decide
@@ -19,15 +22,21 @@ def main():
         images = f['images']
         num_imgs = images.shape[0]
         num_checked = 0
+        ox_list = ['-'] * num_imgs
         idx = 0
         while True:
             cmd = look_and_decide(images[idx])
-            if cmd == '6':
+            if cmd == 'o' or cmd == 'x':
+                ox_list[idx] = cmd
+                print_state(ox_list, idx, num_checked, num_imgs)
+                num_checked += 1
+                idx += 1
+            elif cmd == '6':
                 idx = (idx + 1) % num_imgs
-            if cmd == '4':
-                idx = ((idx - 1) + num_imgs)% num_imgs
-            print(idx,'/',num_imgs, ' | ', 
-                  num_checked,'/',num_imgs)
+                print_state(ox_list, idx, num_checked, num_imgs)
+            elif cmd == '4':
+                idx = ((idx - 1) + num_imgs) % num_imgs
+                print_state(ox_list, idx, num_checked, num_imgs)
         look_and_decide(images[-1])
 
 #import unittest, numpy as np
