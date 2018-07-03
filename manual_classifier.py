346e6b2cc3f60da9f3418d72f0d319ff64233ec4
kur <rhdnfka94@gmail.com>
2018-07-03 21:23:12 +0900

    Load h5 file

--- manual_classifier.py
+++ manual_classifier.py
@@ -1,2 +1,11 @@
+import cv2, h5py
+def main():
+    # load
+    # look_and_decide
+    with h5py.File('./mini_128x_1crop_32.h5','r') as f:
+        images = f['images']
+        for img in images:
+            cv2.imshow('img',img);cv2.waitKey(0)
+
 if __name__ == '__main__':
-    print('man_class')
+    main()
