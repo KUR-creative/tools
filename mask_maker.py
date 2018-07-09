1bb37695a7ca77582eb02b94cef6bb7a69436f77
kur <rhdnfka94@gmail.com>
2018-07-09 22:52:08 +0900

    Add rewrite answer from 'index'(4th arg)

--- mask_maker.py
+++ mask_maker.py
@@ -6,18 +6,26 @@ mask_maker
   make answer data from 'job_records' 
   and then save them into 'answer_dir'
   
+  if you want to start specific 'index' of job records,
+  (want to rewrite answer) pass integer as 4th argument.
+  
 synopsis
   python mask_maker.py job_records answer_dir
+  python mask_maker.py job_records answer_dir index
 
 example
   python mask_maker.py ./job_records.bin ./answers
+  python mask_maker.py ./job_records.bin ./answers 13
 '''
 )
 
 import manual_selector, textMaskMakerUI 
 import sys, os, cv2
 
-def main(job_records_path, answer_dir):
+def worked_job(imgpath_imgname):
+    return (len(imgpath_imgname) == 2)
+
+def main(job_records_path, answer_dir, goto=None):
     now_idx, jobs, selected = manual_selector.load(job_records_path)
 
     try:
@@ -25,9 +33,20 @@ def main(job_records_path, answer_dir):
     except:
         pass # make it anyway!
 
-    for idx,imgpath_imgname in enumerate(selected):
-        if len(imgpath_imgname) == 2:
-            imgpath, imgname = imgpath_imgname
+    print(goto)
+    if goto is None:
+        for idx,imgpath_imgname in enumerate(selected):
+            if worked_job(imgpath_imgname):
+                imgpath, imgname = imgpath_imgname
+                print(idx,imgname)
+                textMaskMakerUI.main(imgpath,
+                                     os.path.join(answer_dir, imgname))
+                selected[idx] = (imgpath, imgname, True)
+                manual_selector.save(now_idx, jobs, selected, job_records_path)
+    else:
+        print('wtf?')
+        for idx,imgpath_imgname in enumerate(selected[goto:]):
+            imgpath, imgname = imgpath_imgname[:2]
             print(idx,imgname)
             textMaskMakerUI.main(imgpath,
                                  os.path.join(answer_dir, imgname))
@@ -35,4 +54,7 @@ def main(job_records_path, answer_dir):
             manual_selector.save(now_idx, jobs, selected, job_records_path)
   
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2])
+    if len(sys.argv) == 1+2:
+        main(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 1+3:
+        main(sys.argv[1], sys.argv[2], int(sys.argv[3]))
