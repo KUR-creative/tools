a19ed8dbeee06f21cb2769eec13d7670672208ea
kur <rhdnfka94@gmail.com>
2018-07-09 22:04:20 +0900

    Add answer_dir argument

--- mask_maker.py
+++ mask_maker.py
@@ -15,15 +15,22 @@ example
 )
 
 import manual_selector, textMaskMakerUI 
-import sys, cv2
+import sys, os, cv2
 
-def main(job_records_path):
+def main(job_records_path, answer_dir):
     now_idx, jobs, selected = manual_selector.load(job_records_path)
+
+    try:
+        os.mkdir(answer_dir)
+    except:
+        pass # make it anyway!
+
     for imgpath, imgname in selected:
         print(imgname)
-        textMaskMakerUI.main(imgpath,imgname)
+        textMaskMakerUI.main(imgpath,
+                             os.path.join(answer_dir,imgname))
         #img = cv2.imread(imgpath)
         #cv2.imshow('img',img); cv2.waitKey(0)                
   
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
