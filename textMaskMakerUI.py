7e0f6b505865094857b7bcdbebea0a626e35b6ee
semuse25 <semuse25@gmail.com>
2018-08-01 11:09:43 +0900

    add mask clearing press 'e'

--- textMaskMakerUI.py
+++ textMaskMakerUI.py
@@ -21,20 +21,23 @@ mainShowArea = windowH - cover
 ix, iy = -1,-1
 drawing = False
 mode = 'RECT'
+modeTmp = 'RECT'
 showMask = False
 img = []
 maskedImg = []
 origin = []
+noChanged=[]
 back = []
 mask = []
 maskBack = []
 maskTemp = []
+maskForClear=[]
 rad = 10
 color = (255,255,255)
 maskColor = (0,0,255)
 
 def textDelete(event, x,y, flags, param):
-    global ix, iy, drawing, img, origin, back, mask, maskBack, maskTemp, mode, rad, color, maskColor
+    global ix, iy, drawing, img, origin, back, mask, maskBack, maskTemp, mode, rad, color, maskColor, maskForClear, noChanged
 
     trace = []
 
@@ -47,6 +50,10 @@ def textDelete(event, x,y, flags, param):
             maskTemp = np.zeros(mask.shape,np.uint8)
             cv2.circle(img,(x,y),rad,color,-1)
             cv2.circle(maskTemp,(x,y),rad,maskColor,-1)
+        elif mode == 'ERASE':
+            maskForClear = np.full(mask.shape,255,np.uint8)
+            #cv2.circle(img,(x,y),rad,(0,0,0),-1)
+            cv2.circle(maskForClear,(x,y),rad,(0,0,0),-1)
         drawing = True
     elif event == cv2.EVENT_MOUSEMOVE:
         if mode == 'RECT':
@@ -57,6 +64,9 @@ def textDelete(event, x,y, flags, param):
             if drawing == True:
                 cv2.circle(img,(x,y),rad,color,-1)
                 cv2.circle(maskTemp,(x,y),rad,maskColor,-1)
+        elif mode == 'ERASE':
+            if drawing == True:
+                cv2.circle(maskForClear,(x,y),rad,(0,0,0),-1)
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
 
@@ -95,6 +105,19 @@ def textDelete(event, x,y, flags, param):
             back = origin.copy()
             origin = img.copy()
             mask = cv2.add(mask,maskTemp)
+        elif mode == 'ERASE':
+            maskBack = mask.copy()
+            maskBack = mask.copy()
+            back = origin.copy()
+            origin = img.copy()
+            mask = cv2.bitwise_and(mask, maskForClear)
+            gmask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+            fmask = cv2.threshold(gmask, 10, 255, cv2.THRESH_BINARY)[1]
+            bmask = cv2.bitwise_not(fmask)
+            fg = cv2.bitwise_and(img, img, mask=fmask)
+            bg = cv2.bitwise_and(noChanged, noChanged, mask=bmask)
+            img = cv2.add(fg,bg)
+            maskForClear = np.full(mask.shape,255,np.uint8)
     elif event == cv2.EVENT_RBUTTONDOWN:
         if mode == 'DRAW' or mode == 'MANUAL':
             color = (img[y,x]).tolist()
@@ -106,7 +129,7 @@ def textDelete(event, x,y, flags, param):
 
 
 def main(srcpath,dstpath) :
-    global img, maskedImg, origin, back, mask, maskBack, drawing, mode, rad , color, showMask, maskColor
+    global img, maskedImg, origin, back, mask, maskBack, drawing, mode, rad , color, showMask, maskColor, maskForClear, noChanged
     Image=cv2.imread(srcpath,cv2.IMREAD_COLOR)
     Mask = np.zeros(Image.shape,np.uint8)
     roiNum = 1
@@ -123,14 +146,16 @@ def main(srcpath,dstpath) :
         else:
             img = Image[mainShowArea*(roiNum-1):]
             mask = Mask[mainShowArea*(roiNum-1):]
-
+        noChanged = img.copy()
         origin = img.copy()
         back = origin.copy()
+        maskForClear = np.full(mask.shape,255,np.uint8)
         cv2.namedWindow('image')
         cv2.setMouseCallback('image',textDelete)
         while True:
             if showMask:
-                gmask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+                m = cv2.bitwise_and(mask,maskForClear)
+                gmask = cv2.cvtColor(m, cv2.COLOR_BGR2GRAY)
                 fmask = cv2.threshold(gmask, 10, 255, cv2.THRESH_BINARY)[1]
                 bmask = cv2.bitwise_not(fmask)
                 fg = cv2.bitwise_and(mask, mask, mask=fmask)
@@ -142,7 +167,7 @@ def main(srcpath,dstpath) :
             #cv2.imshow('mask',mask)
             k = cv2.waitKey(1) & 0xFF
 
-            if k == ord('s'):
+            if k == ord('s') and mode != 'ERASE':
                 if showMask:
                     showMask = False
                     print("mask invisible")
@@ -187,6 +212,15 @@ def main(srcpath,dstpath) :
                 elif mode == 'MANUAL':
                     mode = 'RECT'
                     print('mode RECT')
+            elif k == ord('e') and drawing == False:
+                if mode == 'ERASE':
+                    mode = modeTmp
+                    print('return to ',mode)
+                else:
+                    if showMask == True:
+                        modeTmp = mode
+                        mode = 'ERASE'
+                        print('MASK CLEAR MODE')
             elif k == ord('1') and drawing == False and mode != 'RECT':
                 maskColor = (0,0,255)
                 print('mask color is [RED]: Text on Easy Background')
